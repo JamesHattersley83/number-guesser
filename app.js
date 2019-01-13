a3ede9615b27d834fb94501a6620b6be8502b5ba
jameshattersley83 <jhatts@hotmail.co.uk>
2019-01-13 21:59:30 +0000

    created game variables and selected UI elements

---
+++ app.js
@@ -0,0 +1,13 @@
+// Games values
+let min = 1;
+let max = 10;
+let winningNum = 2;
+let guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
